c1e0c72d64ed662d3b2b5c487f48254ac5fee3e4
Abel h <abelhailu320@gmail.com>
2024-09-03 23:54:37 -0500

    refactored

--- src/MallardDuck.java
+++ src/MallardDuck.java
@@ -3,16 +3,6 @@ public class MallardDuck extends Duck {
     public void display() {
     System.out.println("I'm a mallard duck");
 }
-public void setMallardDuckFlyable(FlyableBehaviour flyableBehaviour){
-        this.flyableBehaviour = flyableBehaviour;
-    flyableBehaviour.fly();
-}
-public void displayFly(){
-    System.out.println(this.flyableBehaviour);
-}
-
-
-
 
 
 }
